24f1d614145f36ff18df5695de250b9b0269ed97
ronnyolssonystad <55841044+ronnyolssonystad@users.noreply.github.com>
2023-12-17 17:46:06 +0100

    Rediredt to edit funkar inte ännu

--- html/php/index.php
+++ html/php/index.php
@@ -1,5 +1,23 @@
 <?php
-include 'db_connect.php';
-phpinfo();
+
+
+$req = array_filter( explode('/', $_SERVER['QUERY_STRING']));
+$url="";
+// request command
+if (isset($req) && count($req) > 0) {
+    $command=$req[1];
+    switch ($command) {
+        case 'edit': $url='./editpers.php';
+    }
+}
+if (isset($req) && count($req) > 1) {
+    $url=$url.'?id='.$req[2];
+}
+if ($url<>"") {
+    echo file_get_contents($url);
+}
+
+
+
 
 ?>
